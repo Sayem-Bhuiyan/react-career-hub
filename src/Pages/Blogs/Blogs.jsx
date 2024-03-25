739301a7cf271dd7d0a00b6329361719bca00928
Sayem <sayem465508@gmail.com>
2024-03-25 11:54:51 +0600

    set up the router

---
+++ src/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,11 @@
+
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>This is Blogs pages</h2>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+import Home from "../Pages/Home/Home";
+import Statistics from "../Pages/Statistics/Statistics";
+import AppliedJobs from "../Pages/AppliedJobs/AppliedJobs";
+import Blogs from "../Pages/Blogs/Blogs";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/statistics',
+                element: <Statistics />
+
+            },
+            {
+                path: 'applied_jobs',
+                element: <AppliedJobs />
+            },
+            {
+                path: '/blogs',
+                element: <Blogs />
+            }
+        ]
+    }
+])
